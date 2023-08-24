052d8c8bc89d198e7bc091526d067f0021c05ffa
iknowca <iknowca@gmail.com>
2023-08-24 16:14:51 +0900

    refactor: [김원기] 로그인 여부를 localstorage를 통해서 알수 있도록 수정 [WMF-10]

--- src/store/user/mutations.js
+++ src/store/user/mutations.js
@@ -1,18 +1,32 @@
 import {
-    REQUEST_USER_LIST_TO_SPRING,
-    REQUEST_USER_TO_SPRING, SET_ACCESS_TOKEN, SET_USER
+  REQUEST_USER_LIST_TO_SPRING,
+  REQUEST_USER_TO_SPRING,
+  SET_ACCESS_TOKEN,
+  SET_SIGNIN,
+  SET_USER
 } from './mutation-types'
+import axiosInst from "@/utility/axiosInstance";
+
 export default {
-    [REQUEST_USER_LIST_TO_SPRING] (state, receiveData) {
-        state.user = receiveData
-    },
-    [REQUEST_USER_TO_SPRING] (state, receiveData) {
-        state.user = receiveData
-    },
-    [SET_ACCESS_TOKEN] (state, receiveData) {
-        state.accessToken = receiveData
-    },
-    [SET_USER] (state, receiveData) {
-        state.user = receiveData
+  [REQUEST_USER_LIST_TO_SPRING](state, receiveData) {
+    state.user = receiveData
+  },
+  [REQUEST_USER_TO_SPRING](state, receiveData) {
+    state.user = receiveData
+  },
+  [SET_ACCESS_TOKEN](state, receiveData) {
+    state.accessToken = receiveData
+  },
+  [SET_USER](state, receiveData) {
+    state.user = receiveData
+  },
+  [SET_SIGNIN](state, receiveData) {
+    if (receiveData) {
+      axiosInst.springAxiosInst.defaults.headers.common.Authorization = `Bearer ${receiveData}`
+      localStorage.setItem("isLogin", true)
+    } else {
+      delete axiosInst.springAxiosInst.defaults.headers.common.Authorization
+      localStorage.clear()
     }
+  }
 }
