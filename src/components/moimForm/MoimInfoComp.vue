a33dca764d24d87d27ac31652232470baedd2f5c
iknowca <iknowca@gmail.com>
2023-08-25 16:08:24 +0900

    feat: [김원기] MOIM 참가 버튼 추가 [WMF-25]

--- src/components/moimForm/MoimInfoComp.vue
+++ src/components/moimForm/MoimInfoComp.vue
@@ -1,23 +1,22 @@
 <template>
 <div>
   {{moim}}
-<!--<JoinBtn v-if="!(user.id in moim.participants?.map((user)=>user.id))"></JoinBtn>-->
+<JoinBtn v-if="!(user.id in moim.participants.map((participant)=>participant.id))"></JoinBtn>
 </div>
 </template>
 
 <script>
 import {useStore} from "vuex";
 import {computed} from "vue";
-// import JoinBtn from "@/components/moimForm/moimComp/JoinBtn.vue";
+import JoinBtn from "@/components/moimForm/moimComp/JoinBtn.vue";
 
 export default {
   name: "MoimInfoComp",
-  // components: {JoinBtn},
+  components: {JoinBtn},
   setup() {
     const store = useStore()
     const moim = computed(()=>store.state.moimModule.moim)
     const user = computed(()=>store.state.userModule.user)
-    console.log(user.value.id in moim.value.participants.map(participant=>participant.id))
 
     return {
       user,
