fca553073d56b7b77bb0856456376b12cc255a0c
iknowca <iknowca@gmail.com>
2023-09-11 16:46:55 +0900

    feat: [김원기] 게시글의 내용을 확인 하는 기능 구현 [WMF-31]

---
+++ src/views/board/BoardReadPage.vue
@@ -0,0 +1,12 @@
+<template>
+<BoardReadComp></BoardReadComp>
+</template>
+
+<script setup>
+
+import BoardReadComp from "@/components/board/BoardReadComp.vue";
+</script>
+
+<style scoped>
+
+</style>
