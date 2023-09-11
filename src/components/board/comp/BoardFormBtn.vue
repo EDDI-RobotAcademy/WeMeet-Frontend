505cc648be6d5a317c90836a24ce7a4f00ec3313
iknowca <iknowca@gmail.com>
2023-09-11 14:44:51 +0900

    feat: [김원기] 게시글 작성 기능 구현 [WMF-29]

---
+++ src/components/board/comp/BoardFormBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn @click="goToFormPage">게시글 작성</v-btn>
+</template>
+
+<script setup>
+import router from "@/router";
+
+// eslint-disable-next-line no-undef
+const props = defineProps({
+  category: String,
+  moimId: {
+    type: Number
+  }
+});
+
+const goToFormPage =()=> {
+  if(props.category==="moim") {
+    router.push(`/board/form/moim/${props.moimId}`)
+  } else {
+    router.push(`/board/form/${props.category}`)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/board/BoardFormPage.vue
@@ -0,0 +1,11 @@
+<template>
+<board-form-comp></board-form-comp>
+</template>
+
+<script setup>
+import BoardFormComp from "@/components/board/BoardFormComp.vue";
+</script>
+
+<style scoped>
+
+</style>
