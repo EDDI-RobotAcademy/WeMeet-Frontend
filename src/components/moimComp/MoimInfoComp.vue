84a37542fed14c723b8788cb08814198e55c214b
iknowca <iknowca@gmail.com>
2023-09-12 12:12:20 +0900

    feat: [김원기] 모임 탈퇴 기능 구현 [WMF-33]

--- src/components/moimComp/MoimInfoComp.vue
+++ src/components/moimComp/MoimInfoComp.vue
@@ -3,6 +3,7 @@
   {{moim}}
   <!--<JoinBtn v-if="!(user.id in moim.participants.map((participant)=>participant.id))"></JoinBtn>-->
   <JoinBtn></JoinBtn>
+  <WithdrawBtn></WithdrawBtn>
  <BoardFormBtn category="moim" :moimId="moim.id"></BoardFormBtn>
 </div>
 </template>
@@ -12,10 +13,11 @@ import {useStore} from "vuex";
 import {computed} from "vue";
 import JoinBtn from "@/components/moimComp/moimComp/JoinBtn.vue";
 import BoardFormBtn from "@/components/board/comp/BoardFormBtn.vue";
+import WithdrawBtn from "@/components/moimComp/moimComp/WithdrawBtn.vue";
 
 export default {
   name: "MoimInfoComp",
-  components: {BoardFormBtn, JoinBtn},
+  components: {WithdrawBtn, BoardFormBtn, JoinBtn},
   setup() {
     const store = useStore()
     const moim = computed(()=>store.state.moimModule.moim)
---
+++ src/components/moimComp/moimComp/WithdrawBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-btn @click="withdraw">
+      withdraw
+    </v-btn>
+  </div>
+</template>
+
+<script setup>
+import {useStore} from "vuex";
+import {computed} from "vue";
+import axiosInstance from "@/utility/axiosInstance";
+import router from "@/router";
+
+const store = useStore()
+const moim = computed(() => store.state.moimModule.moim)
+const getJoinable = (async () => axiosInstance.springAxiosInst.delete(`/moim/${moim.value.id}/user`))
+const withdraw = (async () => getJoinable()
+    .then(() => {
+      router.push(`/`)
+    })
+)
+</script>
+
+<style scoped>
+
+</style>
