18f525a4c0d99045f49b8ae56993b4deb33661ae
iknowca <iknowca@gmail.com>
2023-09-18 11:02:39 +0900

    feat: [김원기] 댓글 삭제기능 구현 [WMF-38]

--- src/components/board/comment/CommentListComp.vue
+++ src/components/board/comment/CommentListComp.vue
@@ -3,6 +3,8 @@
     <v-card>
       <v-card v-for="comment in commentList" :key="comment.id">
         {{comment}}
+        {{ comment.writer.id }}
+        <v-btn v-if="comment.writer.id===userId" @click="deleteComment(comment.id)">delete</v-btn>
       </v-card>
     </v-card>
     <CommentWriteComp></CommentWriteComp>
@@ -10,22 +12,40 @@
 </template>
 
 <script setup>
-import {onMounted, reactive} from "vue";
+import {computed, onMounted, reactive} from "vue";
 import axiosInstance from "@/utility/axiosInstance";
 import {useRoute} from "vue-router";
 import CommentWriteComp from "@/components/board/comment/CommentWriteComp.vue";
+import {useStore} from "vuex";
 
 const route = useRoute()
 const boardId = route.params.boardId
+const store = useStore()
+const userId = computed(()=> {
+  return store.state.userModule.user.id
+})
 
 const commentList = reactive([])
 
 onMounted(()=> {
-  axiosInstance.springAxiosInst.get(`/comment/list/board/${boardId}`)
-    .then(res=>{
-      commentList.push(res.data)
-    })
+  getCommentList()
 })
+
+const getCommentList = ()=> {
+  return axiosInstance.springAxiosInst.get(`/comment/list/board/${boardId}`)
+    .then(res=>{
+      commentList.push(...res.data)
+    })
+}
+
+const deleteComment = (commentId)=> {
+  console.log(commentId)
+  axiosInstance.springAxiosInst.delete(`/comment/${commentId}`)
+    .then(()=>{
+      commentList.length = 0
+      return getCommentList()
+    })
+}
 </script>
 
 <style scoped>
