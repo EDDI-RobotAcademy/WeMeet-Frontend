8c9c98dff9c81476337fbf5036aeb161b7519ff7
iknowca <iknowca@gmail.com>
2023-09-18 11:32:54 +0900

    add: [김원기] 빠진 파일 추가 [WMF-19]

--- src/views/board/BoardWritePage.vue
+++ src/views/board/BoardWritePage.vue
@@ -5,13 +5,19 @@
 <script setup>
 import BoardWriteComp from "@/components/board/BoardWriteComp.vue";
 import axiosInstance from "@/utility/axiosInstance";
+import router from "@/router";
 
 const props = defineProps({
   category: String
 
 })
+
 const submit= (payload)=> {
-  axiosInstance.springAxiosInst.post()
+  payload.category = props.category
+  axiosInstance.springAxiosInst.post(`/board/${props.category}`, payload)
+    .then((res)=>{
+      router.push(`/board/${res.data.board.id}`)
+    })
 }
 </script>
 
