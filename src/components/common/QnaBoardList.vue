5e4baaf6b14dc0f95e63ad286c00001185b6a344
iknowca <iknowca@gmail.com>
2023-09-18 12:54:35 +0900

    upgrade: [김원기] update cycle에서도 변경이 이루어지도록 변경 [WMF-34]

--- src/components/common/QnaBoardList.vue
+++ src/components/common/QnaBoardList.vue
@@ -1,24 +1,33 @@
 <template>
   <BoardListComp :board-list="boardList"></BoardListComp>
+  {{category}}
   <v-btn @click="()=> router.push(`/board/${category}/write`)">글 쓰기</v-btn>
 </template>
 
 <script setup>
 import BoardListComp from "@/components/board/BoardListComp.vue";
-import {onMounted, reactive} from "vue";
+import {computed, onMounted, onUpdated, reactive} from "vue";
 import axiosInstance from "@/utility/axiosInstance";
 import {useRoute} from "vue-router";
 import router from "@/router";
 
 const route = useRoute()
-const category = route.params.category
+const category = computed(()=>route.params.category)
 
 const boardList = reactive([])
-onMounted(() => {
-  axiosInstance.springAxiosInst.get(`/board/list/${category}`, {params: {page: 0, size: 20}})
+
+const getBoardList = ()=> {
+  axiosInstance.springAxiosInst.get(`/board/list/${category.value}`, {params: {page: 0, size: 20}})
     .then((res)=> {
+      boardList.length=0
       boardList.push(...res.data)
     })
+}
+onMounted(() => {
+  getBoardList()
+})
+onUpdated(()=>{
+  getBoardList()
 })
 </script>
 
